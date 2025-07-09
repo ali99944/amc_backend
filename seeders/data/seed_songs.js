785bb95c3bcfce408ceb1a8a572934ed7e2f7470
ali99944 <alitarek99944@gmail.com>
2025-07-09 14:46:00 +0300

    update

--- seeders/data/seed_songs.js
+++ seeders/data/seed_songs.js
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { generateTrackNumber } from "../../lib/random.js";
 
 /**
  * Seeds songs into the database.
@@ -13,62 +14,72 @@ export async function seedSongs(prisma, artists) {
           title: 'Blinding Lights',
           artist_id: artists[0].id,
           is_active: true,
-          genre_id: 1
+          genre_id: 1,
+          track_number: generateTrackNumber()
           
         },
         {
           title: 'Save Your Tears',
           artist_id: artists[0].id,
           is_active: true,
-          genre_id: 1
+          genre_id: 1,
+          track_number: generateTrackNumber()
         },
         {
           title: 'Bad Guy',
           artist_id: artists[1].id,
           is_active: true,
-          genre_id: 1
+          genre_id: 1,
+          track_number: generateTrackNumber()
         },
         {
           title: 'Circles',
           artist_id: artists[2].id,
           is_active: true,
-          genre_id: 2
+          genre_id: 2,
+          track_number: generateTrackNumber()
         },
         {
           title: 'Don’t Know Why',
           artist_id: artists[3].id,
           is_active: true,
-          genre_id: 2
+          genre_id: 2,
+          track_number: generateTrackNumber()
         },
         {
           title: 'One Right Now',
           artist_id: artists[2].id,
           is_active: true,
-          genre_id: 2
+          genre_id: 2,
+          track_number: generateTrackNumber()
         },
         {
           title: 'Slide',
           artist_id: artists[4].id,
           is_active: true,
-          genre_id: 3
+          genre_id: 3,
+          track_number: generateTrackNumber()
         },
         {
           title: 'Feels',
           artist_id: artists[4].id,
           is_active: true,
-          genre_id: 3
+          genre_id: 3,
+          track_number: generateTrackNumber()
         },
         {
           title: 'Everything I Wanted',
           artist_id: artists[1].id,
           is_active: true,
-          genre_id: 3
+          genre_id: 3,
+          track_number: generateTrackNumber()
         },
         {
           title: 'Come Away With Me',
           artist_id: artists[3].id,
           is_active: true,
-          genre_id: 4
+          genre_id: 4,
+          track_number: generateTrackNumber()
         },
       ],
       skipDuplicates: true,
